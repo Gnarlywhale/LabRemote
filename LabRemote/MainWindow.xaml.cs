0aa4b251c4a6e77e18d32a25e264a7f94276bb20
gnarlywhale <gnarlywhale@live.co.uk>
2020-01-11 02:46:48 -0700

    If optitrack aint running, a connection isnt made.

--- LabRemote/MainWindow.xaml.cs
+++ LabRemote/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ using System.Diagnostics;
 using System.Threading;
 using System.IO;
 using NatNetML;
+
 class LSLStream
 {
     public string Name { get; set; }
@@ -42,7 +43,7 @@ namespace LabRemote
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Nat Net Initialization
+        //Nat Net Initialization the address stuff should probably be editable.
         private static NatNetML.NatNetClientML mNatNet;    // The client instance
         private static string mStrLocalIP = "127.0.0.1";   // Local IP address (string)
         private static string mStrServerIP = "127.0.0.1";  // Server IP address (string)
@@ -58,6 +59,7 @@ namespace LabRemote
         private static liblsl.StreamOutlet labRecorderOutlet;
         private static int[] beepSample = new int[1];
         private static int[] triggerSample = new int[1];
+        private static List<string> controlStrings = new List<string>();
 
         public MainWindow()
         {
@@ -74,23 +76,12 @@ namespace LabRemote
             trialTimer = new DispatcherTimer();
             trialTimer.Interval = TimeSpan.FromMilliseconds(1);
             trialTimer.Tick += new EventHandler(trialTimerTick);
+
+            // Setup list of streams that can be controlled, so far just NatNet/Optitrack. Assume that Optitrack is in the name.
+            controlStrings.Add("OptiTrackFrameID");
             refreshStreams(null, null);
 
-            // Move to method that gets called when the "OptiTrack" control checkbox is checked
-            mNatNet = new NatNetML.NatNetClientML();
-            int[] verNatNet = new int[4];
-            verNatNet = mNatNet.NatNetVersion();
-            Console.WriteLine("NatNet SDK Version: {0}.{1}.{2}.{3}", verNatNet[0], verNatNet[1], verNatNet[2], verNatNet[3]);
 
-            /*  [NatNet] Connecting to the Server    */
-            Console.WriteLine("\nConnecting...\n\tLocal IP address: {0}\n\tServer IP Address: {1}\n\n", mStrLocalIP, mStrServerIP);
-
-            NatNetClientML.ConnectParams connectParams = new NatNetClientML.ConnectParams();
-            connectParams.ConnectionType = mConnectionType;
-            connectParams.ServerAddress = mStrServerIP;
-            connectParams.LocalAddress = mStrLocalIP;
-            mNatNet.Connect(connectParams);
-            int a = 2;
         }
 
         private void trialTimerTick(object sender, EventArgs e)
@@ -101,7 +92,7 @@ namespace LabRemote
             if ((Boolean)BeepBox.IsChecked && !beeped && elapsed.TotalSeconds >= Double.Parse(BeepDelay.Text))
             {
                 playBeep(null, null);
-                
+
                 beeped = true;
             }
         }
@@ -113,7 +104,12 @@ namespace LabRemote
             for (int i = 0; i < streams.Length; i++)
             {
                 String name = streams[i].name();
-                LSLstreams.Add(new LSLStream(name, false, streams[i]));
+                Boolean controllable = false;
+                if (controlStrings.Contains(name))
+                {
+                    controllable = true;
+                }
+                LSLstreams.Add(new LSLStream(name, controllable, streams[i]));
             }
         }
         private void refreshStreams(object sender, RoutedEventArgs e)
@@ -124,6 +120,7 @@ namespace LabRemote
         public void populateStreams(object sender, RoutedEventArgs e)
         {
             streamGrid.ItemsSource = LSLstreams;
+
         }
 
         private void SelectProjectPath(object sender, RoutedEventArgs e)
@@ -166,9 +163,10 @@ namespace LabRemote
 
         private void RecordBtn_Click(object sender, RoutedEventArgs e)
         {
+            int resp = 0;
             if (!isRunning)
             {
-                //mNatNet.SendMessageAndWait("SetRecordTakeName")
+
                 isRunning = true;
                 string trialName = TrialName.Text.Replace(".xdf", "") + "_" + trialNum.Text;
                 string fullTrial;
@@ -182,25 +180,32 @@ namespace LabRemote
 
                 }
 
-
-                // Start "Control" of streams
-                int resp = 0;
-                mNatNet.SendMessageAndWait("SetRecordTakeName," + trialName, out resp);
-                mNatNet.SendMessageAndWait("StartRecording", out resp);
-                
-
                 string streamList = " ";
-                
-                foreach(LSLStream lStream in streamGrid.Items)
+
+                foreach (LSLStream lStream in streamGrid.Items)
                 {
-                    if (lStream.Record) streamList += "\"name=\"\""+lStream.Name+"\"\"\" ";
+                    if (lStream.Record) streamList += "\"name=\"\"" + lStream.Name + "\"\"\" ";
+
+                    if (lStream.Controllable)
+                    {
+                        if (lStream.Name.Equals("OptiTrackFrameID")) {
+                            // Start "Control" of streams
+
+                            mNatNet.SendMessageAndWait("SetRecordTakeName," + trialName, out resp);
+
+                            //TODO: Break if recording doesn't start
+                            mNatNet.SendMessageAndWait("StartRecording", out resp);
+
+
+                        }
+                    }
                 }
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.CreateNoWindow = true;
                 startInfo.UseShellExecute = false;
                 startInfo.RedirectStandardInput = true;
                 startInfo.RedirectStandardOutput = true;
-                startInfo.FileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(),"LabRecorderCLI.exe");
+                startInfo.FileName = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "LabRecorderCLI.exe");
                 startInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 startInfo.Arguments = fullTrial + " " + streamList;
 
@@ -220,9 +225,18 @@ namespace LabRemote
             {
                 recorderProcess.StandardInput.Write("\n");
                 recorderProcess.StandardInput.Flush();
-                // Handle stream "Control"
-                int resp;
-                mNatNet.SendMessageAndWait("StopRecording", out resp);
+
+                foreach (LSLStream lStream in streamGrid.Items)
+                {
+                    if (lStream.Controllable)
+                    {
+                        if (lStream.Name.Equals("OptiTrackFrameID"))
+                        {
+                            mNatNet.SendMessageAndWait("StopRecording", out resp);
+                            //TODO: Display message if recording wasn't able to stop recording
+                        }
+                    }
+                }
 
                 RecordBtn.Content = "Start Trial";
                 trialTimer.Stop();
@@ -235,5 +249,32 @@ namespace LabRemote
         {
             labRecorderOutlet.push_sample(triggerSample);
         }
+
+        private void ControlBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            System.Windows.Controls.CheckBox ckbox = sender as System.Windows.Controls.CheckBox;
+            LSLStream curStream = ckbox.DataContext as LSLStream;
+            if (curStream.Controllable)
+            {
+                if (curStream.Name.Equals("OptiTrackFrameID"))
+                {
+                    // Move to method that gets called when the "OptiTrack" control checkbox is checked
+                    mNatNet = new NatNetML.NatNetClientML();
+                    int[] verNatNet = new int[4];
+                    verNatNet = mNatNet.NatNetVersion();
+                    Console.WriteLine("NatNet SDK Version: {0}.{1}.{2}.{3}", verNatNet[0], verNatNet[1], verNatNet[2], verNatNet[3]);
+
+                    /*  [NatNet] Connecting to the Server    */
+                    Console.WriteLine("\nConnecting...\n\tLocal IP address: {0}\n\tServer IP Address: {1}\n\n", mStrLocalIP, mStrServerIP);
+
+                    NatNetClientML.ConnectParams connectParams = new NatNetClientML.ConnectParams();
+                    connectParams.ConnectionType = mConnectionType;
+                    connectParams.ServerAddress = mStrServerIP;
+                    connectParams.LocalAddress = mStrLocalIP;
+                    mNatNet.Connect(connectParams);
+                }
+            }
+        } 
     }
 }
